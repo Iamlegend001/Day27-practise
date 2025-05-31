4f4ce1c70a0381200f6e1171eaef791e10522239
Arka <arkapravasantra17@gmail.com>
2025-05-31 21:31:55 +0530

    meternity

---
+++ app.js
@@ -0,0 +1,37 @@
+// let arr1 = [1, 2, 3];
+// let arr2 = [4, 5, 6];
+
+// let ans = new Array(arr1.length + arr2.length);
+
+// let i = 0;
+// let k = 0;
+// let j = 0;
+
+// while (i < arr1.length && j < arr2.length) {
+//   if (arr1[i] < arr2[j]) {
+//     ans[k++] = arr1[i++];
+//   } else {
+//     ans[k++] = arr2[j++];
+//   }
+
+//   while (i < arr1.length) {
+//     ans[k++] = arr1[i++];
+//   }
+
+//   while (j < arr2.length) {
+//     ans[k++] = arr2[j++];
+//   }
+
+//   console.log(ans);
+// }
+
+// Merge sorted array
+
+let num1 = [1, 2, 3, 0, 0, 0];
+let num2 = [2, 5, 6];
+
+
+let i = m -1 , j = n-1 , k = num1.length -1
+while(i>= 0 && j >=0){
+  
+}
